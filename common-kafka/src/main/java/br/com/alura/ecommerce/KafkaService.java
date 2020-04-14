9d792dd3c1c9ee75a9d193e40e003748743852ca
Fabricio Barufi <fabricio.barufi@zup.com.br>
2020-04-14 17:43:12 -0300

    consumer de fraud consome e produz mensagens

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
 
 class KafkaService<T> implements Closeable {
@@ -46,7 +47,15 @@ class KafkaService<T> implements Closeable {
                 System.out.println("Encontrei " + records.count() + " registros");
 
                 for (var record : records) {
-                    parse.consume(record);
+                    try {
+                        parse.consume(record);
+                    } catch (ExecutionException e) {
+                        //so far, justing logging the exception for this message
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        //so far, justing logging the exception for this message
+                        e.printStackTrace();
+                    }
                 }
             }
         }
--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -2,7 +2,9 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 public class FraudDetectorService {
 
@@ -20,7 +22,9 @@ public class FraudDetectorService {
         }
     }
 
-    private void parse(ConsumerRecord<String, Order> record) {
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new Order, checking for fraud");
         System.out.println(record.key());
@@ -34,6 +38,26 @@ public class FraudDetectorService {
             //ignoring: é só uma simulação de tempo
             e.printStackTrace();
         }
-        System.out.println("Order processed");
+
+        var order = record.value(); //devolve uma order
+
+        if (isFraud(order)) {
+            //pretending that the fraud happens when the amount is >= 4500
+            System.out.println("Order is a Fraud!!!!");
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED",
+                    order.getUserId(),
+                    order);
+        } else {
+            System.out.println("Approved " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED",
+                    order.getUserId(),
+                    order);
+        }
+
+
+    }
+
+    private boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
     }
 }
