3d302ba97866d61ab598b74be52af9cadd14e2fb
Fabricio Barufi <fabricio.barufi@zup.com.br>
2020-04-17 12:20:57 -0300

    Server http embedded, new service http, send msg via server http

--- service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -8,24 +8,6 @@ public class NewOrderMain{
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<String>()) {
 
-                for (var i = 0; i < 10; i++) {
-
-                    var orderId = UUID.randomUUID().toString();
-                    var amount = new BigDecimal(Math.random() * 5000 + 1);
-                    var email = Math.random() + "@email.com";
-
-                    var order = new Order(orderId, amount, email);
-
-                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
-
-                    var emailCode = "Thank tou for your order! We are processing your order!";
-                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
-
-                }
-            }
-        }
     }
 }
